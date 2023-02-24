9a2c10a03991548bd8941315b3397916be66c36c
FIRST_NAME LAST_NAME <xhuagf299@gmail.com>
2023-02-24 07:04:47 +0700

    try1

---
+++ lib/screens/home_screen.dart
@@ -0,0 +1,90 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:notesapp/screens/note_editor.dart';
+import 'package:notesapp/screens/note_reader.dart';
+import 'package:notesapp/style/app_style.dart';
+import 'package:notesapp/widgets/note_card.dart';
+
+class HomeScreen extends StatefulWidget {
+  const HomeScreen({super.key});
+
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: AppStyle.mainColor,
+      appBar: AppBar(
+        elevation: 0.0,
+        title: Text("Notes App"),
+        centerTitle: true,
+        backgroundColor: AppStyle.mainColor,
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.start,
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Text(
+              "Your Notes: ",
+              style: GoogleFonts.inter(
+                  color: Colors.white,
+                  fontWeight: FontWeight.normal,
+                  fontSize: 18),
+            ),
+            SizedBox(
+              height: 20.0,
+            ),
+            Expanded(
+              child: StreamBuilder<QuerySnapshot>(
+                stream:
+                    FirebaseFirestore.instance.collection("Notes").snapshots(),
+                builder: (context, AsyncSnapshot<QuerySnapshot> snapshot) {
+                  if (snapshot.connectionState == ConnectionState.waiting) {
+                    return Center(
+                      child: CircularProgressIndicator(),
+                    );
+                  }
+
+                  if (snapshot.hasData) {
+                    return GridView(
+                      gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                          crossAxisCount: 2),
+                      children: snapshot.data!.docs
+                          .map((note) => noteCard(() {
+                                Navigator.push(
+                                    context,
+                                    MaterialPageRoute(
+                                      builder: (context) =>
+                                          NoteReaderScreen(note),
+                                    ));
+                              }, note))
+                          .toList(),
+                    );
+                  }
+                  return Text(
+                    "You don't have a note",
+                    style: GoogleFonts.inter(color: Colors.white),
+                  );
+                },
+              ),
+            )
+          ],
+        ),
+      ),
+      floatingActionButton: FloatingActionButton.extended(
+        onPressed: (() {
+          Navigator.push(context,
+              MaterialPageRoute(builder: (context) => NoteEditorScreen()));
+        }),
+        label: Text("Add Note"),
+        icon: Icon(Icons.add),
+      ),
+    );
+  }
+}
---
+++ lib/style/app_style.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+class AppStyle{
+
+  static Color mainColor = Color.fromARGB(255, 152, 168, 248);
+  static Color accentColor = Color.fromARGB(255, 179, 192, 255);
+
+
+  static List<Color> cardsColor = [
+    Colors.white,
+    Colors.lightBlue,
+    Colors.red,
+    Colors.green,
+    Colors.grey,
+    Colors.cyan
+  ];
+
+
+   static TextStyle mainTitle = GoogleFonts.inter(
+    fontSize: 18.0,
+    fontWeight: FontWeight.normal
+   );
+
+   static TextStyle mainContent = GoogleFonts.inter(
+    fontSize: 14.0,
+    fontWeight: FontWeight.normal
+   );
+}
